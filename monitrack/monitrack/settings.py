3c75d4021dac302419f26f6e46235c954af995c7
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-05-23 18:29:31 +0530

    something

--- monitrack/monitrack/settings.py
+++ monitrack/monitrack/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')] #join our base directory and static files and store it in static files directory. 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
