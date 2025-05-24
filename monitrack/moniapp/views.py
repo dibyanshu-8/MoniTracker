2256e5140962d0925279561201efb14ce4c2417e
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-05-25 01:44:05 +0530

    importing in views.py

--- monitrack/moniapp/views.py
+++ monitrack/moniapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm
+from moniapp import models
+from.models import Account,Expense
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from django.views.generic import ListView 
+from datetime import datetime
 
 # Create your views here.
+
