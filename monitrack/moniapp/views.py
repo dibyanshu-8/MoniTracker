7869d15f26c4e332a6659eff345e17e38f57929e
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-05-25 02:02:06 +0530

    function for home and register page

--- monitrack/moniapp/views.py
+++ monitrack/moniapp/views.py
@@ -11,4 +11,16 @@ from django.views.generic import ListView
 from datetime import datetime
 
 # Create your views here.
+def home(request):
+    return render(request,'home/home.html')
+def register(request):
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid:
+            user=form.save()
+            login(request,user)
+            return redirect('home')
+    else:
+        form=UserCreationForm()
+    return render(request,'registration/register.html',{'form':form})
 
