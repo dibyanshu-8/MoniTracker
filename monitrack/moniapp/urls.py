a73ffafeb2b29a8f06a1c3409e8b66fe845f4784
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-05-26 00:20:50 +0530

    changes in app's urls.py & adding paths to urls

--- monitrack/moniapp/urls.py
+++ monitrack/moniapp/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path,include
 from .import views
 
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('accounts/register/',views.register,name='register'),
+    path('accounts/',include('django.contrib.auth.urls')),
+]
