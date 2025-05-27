3f78baabedd20c41803ff50799b735eb644a2bb6
Dibyanshu kar <dibyanshukar8@gmail.com>
2025-05-27 19:06:58 +0530

    code in forms.py

--- monitrack/moniapp/forms.py
+++ monitrack/moniapp/forms.py
@@ -3,4 +3,18 @@ from .models import Expense
 
 
 class ExpenseForm(forms.ModelForm):
+    long_term=forms.BooleanField(required=False)
     
+    
+    class Meta:
+        model=Expense
+        fields=['name','amount','interest_rate','date','end_date','long_term']
+        
+        widgets= {
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'amount':forms.NumberInput(attrs={'class':'form-control'}),
+            'date':forms.DateInput(attrs={'class':'form-control','type':'date'}),
+            'interest_rate':forms.NumberInput(attrs={'class':'form-control'}),
+            'end_date':forms.DateInput(attrs={'class':'form-control','type':'date'}),
+            'long_term':forms.CheckboxInput(attrs={'class':'form-control'}),
+        }
